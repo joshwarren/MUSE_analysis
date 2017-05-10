a9cb5d45e8a7c47d9feadc0b6aac0284006eece9
joshwarren <joshua.warren@physics.ox.ac.uk>
2017-05-10 17:43:27 +0100

    New Routine for plotting BPT diagram of spaxels
    BPT.py: new routine to be called by full_analysis2_muse.py for placing each bin on the BPT diagram, classifying them depending on where they fall and then coloring them to give a map of the AGN.
    
     On branch master
     Your branch is up-to-date with 'joshwarren/master'.
    
     Changes to be committed:
    	new file:   BPT.py

---
+++ BPT.py
@@ -0,0 +1,79 @@
+from checkcomp import checkcomp
+cc = checkcomp()
+if 'home' not in cc.device:
+	import matplotlib # 20160202 JP to stop lack-of X-windows error
+	matplotlib.use('Agg') # 20160202 JP to stop lack-of X-windows error
+import cPickle as pickle
+import matplotlib.pyplot as plt
+import numpy as np
+from plot_velfield_nointerp import plot_velfield_nointerp
+
+
+
+
+def BPT(galaxy, D=None):
+	print '   BPT'
+
+	analysis_dir = "%s/Data/muse/analysis" % (cc.base_dir)
+	galaxiesFile = "%s/galaxies.txt" % (analysis_dir)
+
+	output = '%s/%s/results' % (analysis_dir, galaxy)
+
+
+	if D is None:
+		pickleFile = open('%s/pickled/dataObj_.pkl' % (output))
+		D = pickle.load(pickleFile)
+		pickleFile.close()
+
+	for bin in D.bin:
+		for e in bin.e_line.itervalues():
+			e.__threshold__ = 0
+# ------------=============== BPT diagram =================----------
+	fig, ax = plt.subplots()
+	x = np.log(D.e_line['[OIII]5007d'].flux/D.e_line['Halpha'].flux)
+	y = np.log(D.e_line['[SII]6716'].flux/D.e_line['Hbeta'].flux)
+
+	Seyfert2 = (0.72/(x - 0.32) + 1.30 < y) * (1.89 * x + 0.76 < y)
+	LINER = ((0.72/(x - 0.32) + 1.30 < y) * (1.89 * x + 0.76 > y)) + (x > 0.32)
+	SF = (0.72/(x - 0.32) + 1.30 > y) - (x > 0.32)
+	# o = (1.89 * x + 0.76 > y) * (x > 0.32)
+
+	ax.scatter(x[Seyfert2], y[Seyfert2], c='r')
+	ax.scatter(x[LINER], y[LINER], c='g')
+	ax.scatter(x[SF], y[SF], c='b')
+	# ax.scatter(x[o],y[o],c='k')
+
+	x_line1 = np.arange(-2.0, 0.1, 0.001)
+	y_line1 = 0.72/(x_line1 - 0.32) + 1.30
+	ax.plot(x_line1, y_line1,'k')
+
+
+	y_line2 = 1.89 * x_line1 + 0.76
+
+	m = y_line2 > y_line1
+	a = np.min(x_line1[m])
+	x_line2 = np.arange(a, 0.60, 0.001)
+	y_line2 = 1.89 * x_line2 + 0.76
+
+	ax.plot(x_line2, y_line2, 'k')
+
+	ax.set_xlim([-1.2, 0.7])
+	ax.set_ylim([-1.2, 1.5])
+
+	fig.savefig('/Data/MUSE/analysis/%s/results/plots/BPT.png' % (galaxy))
+# ------------================= BPT map ===================----------
+	m = np.ones(D.number_of_bins)*np.nan
+	m[Seyfert2] = +1
+	m[LINER] = 0
+	m[SF] = -1
+
+	saveTo = '/Data/MUSE/analysis/%s/results/plots/AGN_map.png' % (galaxy)
+	plot_velfield_nointerp(D.x, D.y, D.bin_num, D.xBar, D.yBar, m, vmin=-1.3, vmax=1.3,
+		nodots=True, title='Map of AGN', save=saveTo, res=0.2)
+
+
+	return D
+
+
+if __name__=='__main__':
+	BPT('ic1459')
