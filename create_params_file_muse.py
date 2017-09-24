af5ad34110428c1bf0bbb0471165a926115117a4
joshwarren <joshua.warren@physics.ox.ac.uk>
2017-09-24 18:02:52 +0100

    EoDC: Found error in final run if temp_mismatch = True
    errors2_muse.py: Corrected mistake where self._ should read self. in run_ppxf routine if params.temp_mismatch is set.
    
    create_params_file_muse.py: General use
    
     On branch master
     Your branch is up-to-date with 'joshwarren/master'.
    
     Changes to be committed:
    	modified:   create_params_file_muse.py
    	modified:   errors2_muse.py

--- create_params_file_muse.py
+++ create_params_file_muse.py
@@ -2,11 +2,10 @@ import numpy as np
 from checkcomp import checkcomp
 cc = checkcomp()
 
-opt = 'pop_sauron_method' 	# must contain kin or pop
+opt = 'pop' 	# must contain kin or pop
 
 galaxies = ['ic1459', 'ic4296', 'ngc1316', 'ngc1399']
 gals=[0,1,2,3]
-gals=[2]
 
 output_file = "%s/MUSE/analysis/params.txt" % (cc.home_dir)
 f = open(output_file, 'w')
@@ -25,20 +24,20 @@ for gal in gals:
 
 	f.write("push.sh 'Glamdring MUSE finished %s'\n"%(galaxy))
 
-# if 'pop' in opt:
-# 	for gal in gals:
-# 		galaxy = galaxies[gal]
+if 'pop' in opt:
+	for gal in gals:
+		galaxy = galaxies[gal]
 
-# 		tessellation_File = '%s/Data/muse/analysis/%s/%s/' % (cc.base_dir, galaxy, opt) + \
-# 			'setup/voronoi_2d_binning_output.txt' 
+		tessellation_File = '%s/Data/muse/analysis/%s/%s/' % (cc.base_dir, galaxy, opt) + \
+			'setup/voronoi_2d_binning_output.txt' 
 
-# 		bin_num = np.loadtxt(tessellation_File, usecols=(2,), skiprows=1, unpack=True)
-# 		n_bins = int(max(bin_num)+1)
+		bin_num = np.loadtxt(tessellation_File, usecols=(2,), skiprows=1, unpack=True)
+		n_bins = int(max(bin_num)+1)
 
-# 		for i in range(n_bins):
-# 			# if 'kin' in opt:
-# 			f.write("python pop.py muse " + str(gal) + " " + opt + " " + str(i) + 
-# 				"\n")
+		for i in range(n_bins):
+			# if 'kin' in opt:
+			f.write("python pop.py muse " + str(gal) + " " + opt + " " + str(i) + 
+				"\n")
 f.write("push.sh 'Glamdring MUSE run finished'")
 
 f.close()
