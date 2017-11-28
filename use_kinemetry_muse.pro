a450a2bcc85f738c1c3767943eb3f9cda591e3d2
joshwarren <joshua.warren@physics.ox.ac.uk>
2017-11-28 00:16:48 +0000

    EoDC:
    use_kinemetry_muse.[pro, py]: General use
    
     On branch master
     Your branch is up-to-date with 'joshwarren/master'.
    
     Changes to be committed:
    	modified:   use_kinemetry_muse.pro
    	modified:   use_kinemetry_muse.py

--- use_kinemetry_muse.pro
+++ use_kinemetry_muse.pro
@@ -18,7 +18,8 @@ PRO do_work, gal, opt, type
 	if strcmp(type, 'flux', 4, /FOLD_CASE) then er_velbin = SQRT(velbin)
 
 	; Read in binning
-	file = '/Data/muse/analysis/'+gal+'/'+opt+'/setup/voronoi_2d_binning_output.txt'
+	file = '/Data/muse/analysis/'+gal+'/'+opt+$
+		'/setup/voronoi_2d_binning_output.txt'
 	rdfloat, file, _,_,bin_num, xbin, ybin, skipline=1
 
 	file = '/Data/muse/analysis/galaxies.txt'
@@ -50,36 +51,68 @@ PRO do_work, gal, opt, type
 
 	; NB: gas must be the first 3 characters in type
 	if strcmp(type, 'gas', 3, /FOLD_CASE) then begin
+
+		; file = '/Data/muse/analysis/'+gal+'/'+opt+'/kinemetry/gas_flux.dat'
+		; ; read in field
+		; rdfloat, file, flux, er_flux
+
+		; file = '/Data/muse/analysis/'+gal+'/'+opt+$
+		; 	'/setup/voronoi_2d_binning_output.txt'
+		; rdfloat, file, _,_,bin_num2, xbin2, ybin2, skipline=1
+		; goodpix = where(flux ne 9999)
+
+		; b = uniq(bin_num1,sort(bin_num2))
+		; xbin2 = xbin2[b]
+		; ybin2 = ybin2[b]
+
+		; ; Center the origin on the center of the galaxy
+		; xbin2 = xbin2 - x0
+		; ybin2 = ybin2 - y0
+
+		; xbin2 = xbin2[goodpix]
+		; ybin2 = ybin2[goodpix]
+		; flux = flux[goodpix]
+
+		; KINEMETRY, xbin2, ybin2, flux, rad, pa, q, cf, $
+		; 	ntrm=6, scale=0.2, $; /FIXCEN, 
+		; 	even=1, $
+		; 	plot_dir='/Data/muse/analysis/'+gal+'/'+opt+$
+		; 	'/kinemetry/kinemetry_gas_flux.jpeg'
+
 		; Get average PA
-		KINEMETRY, xbin, ybin, velbin, rad, pa, q, cf, x0=x0, $
-			y0=y0, ntrm=2, scale=0.2, name=gal,er_cf=er_cf, $
-			er_pa=er_pa, even=even, ERROR=er_velbin, er_q=er_q, badpix=badpix, $
-			velkin=velkin, velcirc=velcirc, /bmodel, cover=0.05, /FIXCEN
+		KINEMETRY, xbin, ybin, velbin, rad, pa, q, cf, $
+			ntrm=6, scale=0.2, $; /FIXCEN, 
+			even=even, ERROR=er_velbin, $
+			er_pa=er_pa, er_cf=er_cf, er_q=er_q, velkin=velkin, $
+			velcirc=velcirc, /bmodel, $;paq=[pa,q], radius=rad/0.2, $
+			plot_dir='/Data/muse/analysis/'+gal+'/'+opt+'/kinemetry/kinemetry_'+$
+			type+'.jpeg'
+	; 	med_pa  = MEDIAN(pa)
+	; 	;if even eq 0 then 
+	; 	; med_pa = med_pa - 90
 
-		med_pa  = MEDIAN(pa)
-		;if even eq 0 then 
-		med_pa = med_pa - 90
+	; 	med_q = MEDIAN(q)
 
-		med_q = MEDIAN(q)
-
-	; Low harmonic terms only
-		KINEMETRY, xbin, ybin, velbin, rad, pa, q, cf, x0=x0, $
-			y0=y0, ntrm=2, scale=0.2, name=gal,er_cf=er_cf, $
-			er_pa=er_pa, even=even, ERROR=er_velbin, er_q=er_q, badpix=badpix, $
-			velkin=velkin, velcirc=velcirc, /bmodel, cover=0.05, /FIXCEN, $
-			rangepa=[med_pa,med_pa+0.1], rangeq=[med_q,med_q+0.1]
-
+	; ; Low harmonic terms only
+	; 	KINEMETRY, xbin, ybin, velbin, rad, pa, q, cf, $
+	; 		ntrm=6, scale=0.2, $; /FIXCEN, 
+	; 		even=even, ERROR=er_velbin, $
+	; 		er_pa=er_pa, er_cf=er_cf, er_q=er_q, velkin=velkin, $
+	; 		velcirc=velcirc, /bmodel
 		
-		file = '/Data/muse/analysis/'+gal+'/'+opt+'/kinemetry/kinemetry_'+type+'_2Dmodel.txt'
+		file = '/Data/muse/analysis/'+gal+'/'+opt+'/kinemetry/kinemetry_'+$
+			type+'_2Dmodel.txt'
 		forprint2, xbin, ybin, velkin, velcirc, width=200, TEXTOUT=file, /SILENT, $
 			comment='   xbin      ybin       velkin    velcirc'
 
 		k1 = SQRT(cf[*,1]^2 + cf[*,2]^2)
 		erk1 = (SQRT( (cf[*,1]*er_cf[*,1])^2 + (cf[*,2]*er_cf[*,2])^2 ))/k1
 
-		file = '/Data/muse/analysis/'+gal+'/'+opt+'/kinemetry/kinemetry_'+type+'.txt'
-		forprint2, rad, pa, er_pa, q, er_q, k1, erk1, width=200, TEXTOUT = file, $
-			/SILENT, comment='  radius(arcsec)      pa(deg)        err         ellip        err           k1           err'
+		file = '/Data/muse/analysis/'+gal+'/'+opt+'/kinemetry/kinemetry_'+$
+			type+'.txt'
+		forprint2, rad, pa, er_pa, 1-q, er_q, k1, erk1, width=200, $
+			TEXTOUT = file, /SILENT, comment='  radius(arcsec)      pa(deg)   '+$
+			'     err         ellip        err           k1           err'
 	endif
 
 	; Include high harmonic terms to reach k5.
@@ -94,8 +127,10 @@ PRO do_work, gal, opt, type
 		
 		KINEMETRY, xbin, ybin, velbin, rad, pa, q, cf, $;x0=x0, y0=y0, $
 			ntrm=ntrm, scale=0.2, /FIXCEN, even=even, error=er_velbin, $
-			er_pa=er_pa, er_q=er_q, er_cf=er_cf, $;cover=0.05,$
-			plot_dir='/Data/muse/analysis/'+gal+'/'+opt+'/kinemetry/kinemetry_'+type+'.jpeg'
+			er_pa=er_pa, er_q=er_q, er_cf=er_cf, velkin=velkin, $
+			velcirc=velcirc, /bmodel, $;cover=0.05,$
+			plot_dir='/Data/muse/analysis/'+gal+'/' + opt + $
+			'/kinemetry/kinemetry_'+type+'.jpeg'
 
 		if stregex(type, '.*_vel', /boolean) then begin
 			k0 = cf[*,0]
@@ -106,21 +141,33 @@ PRO do_work, gal, opt, type
 			erk5 = (SQRT( (cf[*,5]*er_cf[*,5])^2 + (cf[*,6]*er_cf[*,6])^2 ))/k5
 			erk51 = ( SQRT( ((k5/k1) * erk1)^2 + erk5^2  ) )/k1 
 
-			file = '/Data/muse/analysis/'+gal+'/'+opt+'/kinemetry/kinemetry_'+type+'.txt'
-			forprint2, rad, pa, er_pa, q, er_q, k1, erk1, k51, erk51, width=200, TEXTOUT = file, $
-				/SILENT, comment='  radius(arcsec)      pa(deg)        err         ellip        err           k1           err          k51         err'
-
+			file = '/Data/muse/analysis/'+gal+'/' + opt + $
+				'/kinemetry/kinemetry_'+type+'.txt'
+			forprint2, rad, pa, er_pa, q, er_q, k1, erk1, k51, erk51, width=200, $
+				TEXTOUT = file, /SILENT, comment='  radius(arcsec)      pa(deg)  '+$
+				'      err         ellip        err           k1           err   '+$
+				'       k51         err'
 		endif
 
 		if stregex(type, '.*_flux', /boolean) then begin
 			b4 = cf[*, 8]
 			er_b4 = er_cf[*, 8]
 			
-			file = '/Data/muse/analysis/'+gal+'/'+opt+'/kinemetry/kinemetry_'+type+'.txt'
+			file = '/Data/muse/analysis/'+gal+'/' + opt + $
+				'/kinemetry/kinemetry_'+type+'.txt'
 			forprint2, rad, pa, er_pa, 1-q, er_q, b4, er_b4, width=200, $
 				TEXTOUT = file, /SILENT, $
-				comment='  radius(arcsec)      pa(deg)        err         ellip        err           b4           err'
+				comment='  radius(arcsec)      pa(deg)        err         ellip  '+$
+				'      err           b4           err'
 		endif
+
+		if stregex(type, '.*_vel', /boolean) then begin
+			file = '/Data/muse/analysis/'+gal+'/' + opt + $
+				'/kinemetry/kinemetry_'+type+'_2Dmodel.txt'
+			forprint2, xbin, ybin, velkin, velcirc, width=200, TEXTOUT=file, $
+				/SILENT, comment='   xbin      ybin       velkin    velcirc'
+		endif
+
 	endif
 
 	; plot=0
@@ -139,11 +186,15 @@ PRO do_work, gal, opt, type
 	; 	if type eq 'stellar_vel' then !p.multi=[0,1,4] else !p.multi=[0,1,3]
 	; 	!Y.MARGIN=[0,0] ; show plots with shared X axis
 	; 	!Y.OMARGIN=[5,3] ; allow for space for the axis labels
-	; 	ploterror, rad, pa, er_pa, PSYM=-5, TITLE=gal, xtickformat = '(A1)', YTITLE='!7C!X!N [degrees]', YRANGE=[min(pa),max(pa)]
-	; 	ploterror, rad, q, er_q, PSYM=-5, YRANGE=[0,1.1], xtickformat = '(A1)', YTITLE='q'
-	; 	ploterror, rad, k1, erk1, PSYM=-5, xtickformat = '(A1)', YTITLE='k1 [km/s]',YRANGE=[0,245]
+	; 	ploterror, rad, pa, er_pa, PSYM=-5, TITLE=gal, xtickformat = '(A1)', $
+	; 		YTITLE='!7C!X!N [degrees]', YRANGE=[min(pa),max(pa)]
+	; 	ploterror, rad, q, er_q, PSYM=-5, YRANGE=[0,1.1], xtickformat = '(A1)', $
+	; 		YTITLE='q'
+	; 	ploterror, rad, k1, erk1, PSYM=-5, xtickformat = '(A1)', $
+	; 		YTITLE='k1 [km/s]',YRANGE=[0,245]
 	; 	if type eq 'stellar_vel' then begin
-	; 		ploterror, rad, k51, erk51, PSYM=-5, XTITLE='R [arcsec]', YTITLE='k5/k1', YRANGE=[0,0.13]
+	; 		ploterror, rad, k51, erk51, PSYM=-5, XTITLE='R [arcsec]', $
+	; 		YTITLE='k5/k1', YRANGE=[0,0.13]
 	; 	endif
 	; 	!P.MULTI=0
 	; 	!Y.MARGIN=[4,2] ; back to default values
@@ -162,23 +213,26 @@ PRO do_work, gal, opt, type
 	; 	image24[1,*,*] = g[snapshot]
 	; 	image24[2,*,*] = b[snapshot]
 
-	; 	Write_JPEG, '/Data/muse/analysis/'+gal+'/'+opt+'/kinemetry/kinemetry_'+type+'.jpeg', image24, True=1, Quality=75
+	; 	Write_JPEG, '/Data/muse/analysis/'+gal+'/'+opt+'/kinemetry/kinemetry_'+$
+	; 		type+'.jpeg', image24, True=1, Quality=75
 	; endif
 
 END
 
 
 pro use_kinemetry_muse
-	gals=['ic1459', 'ic4296', 'ngc1316', 'ngc1399']
-	; gals=['ngc1316']
-	for i=0,3 do begin
-		gal=gals[i]
-		do_work, gal, 'kin', 'stellar_flux'
-		; do_work, gal, 'kin3', 'stellar_vel'
-		; do_work, gal, 'kin3', 'gas_flux'
-		; do_work, gal, 'kin3', 'gas_vel'
-		; do_work, gal, 'kin3', 'gas_sigma'
+	; do_work, 'ngc1316', 'pop', 'gas_vel'
+	do_work, 'ngc1316', 'pop', 'stellar_vel'
+	; gals=['ic1459', 'ic4296', 'ngc1316', 'ngc1399']
+	; ; gals=['ngc1316']
+	; for i=0,3 do begin
+	; 	gal=gals[i]
+	; 	do_work, gal, 'kin', 'stellar_flux'
+	; 	; do_work, gal, 'kin3', 'stellar_vel'
+	; 	; do_work, gal, 'kin3', 'gas_flux'
+	; 	; do_work, gal, 'kin3', 'gas_vel'
+	; 	; do_work, gal, 'kin3', 'gas_sigma'
 
-	endfor
+	; endfor
 
 END
--- use_kinemetry_muse.py
+++ use_kinemetry_muse.py
@@ -15,128 +15,131 @@ from rolling_stats import rollmed
 from plot_velfield_nointerp import plot_velfield_nointerp
 from astropy.io import fits
 from errors2_muse import get_dataCubeDirectory
-from plot_results_muse import set_lims
+from plot_results_muse import set_lims, add_
 
-def use_kinemetry(gal, opt='kin'):
+def use_kinemetry(gal, opt='pop'):
 	out_dir = '%s/Data/muse/analysis' % (cc.base_dir)
-	colors=['b','y','g']
-	fig, ax = plt.subplots()
-	pl_ob=[] # array for the plot objects
-	missing=0 # number of missing plots (out of 3)
+	# colors=['b','y','g']
+	# fig, ax = plt.subplots()
+	# pl_ob=[] # array for the plot objects
+	# missing=0 # number of missing plots (out of 3)
 
 	# Plot all avaliable types
-	for i, type in enumerate(['flux','vel','sigma']):
-		f = '%s/%s/%s/kinemetry/kinemetry_gas_%s.txt' % (out_dir, gal, opt, type)
-		if os.path.exists(f):
-			rad, pa, er_pa, q, er_q, k1, erk1 = np.loadtxt(f, unpack=True, skiprows=1)
-			pa = rollmed(pa, 5)
-			k1 = rollmed(k1, 5)
-			# rad*=0.2 # Change to arcsec
+	# for i, type in enumerate(['flux','vel','sigma']):
+	# 	f = '%s/%s/%s/kinemetry/kinemetry_gas_%s.txt' % (out_dir, gal, opt,
+	# 		type)
+	# 	if os.path.exists(f):
+	# 		rad, pa, er_pa, q, er_q, k1, erk1 = np.loadtxt(f, unpack=True, 
+	#			skiprows=1)
+	# 		pa = rollmed(pa, 5)
+	# 		k1 = rollmed(k1, 5)
+	# 		# rad*=0.2 # Change to arcsec
 
-			# Align pa[0] as closely as possible with flux pa[0] by +/- 360 deg
-			if type == 'flux': pa0 = pa[0]
-			else:
-				a = np.argmin(np.abs(pa[0]-pa0+[-360,0,360]))
-				if a == 0:
-					pa -= 360
-				elif a == 2:
-					pa += 360
+	# 		# Align pa[0] as closely as possible with flux pa[0] by +/- 360 deg
+	# 		if type == 'flux': pa0 = pa[0]
+	# 		else:
+	# 			a = np.argmin(np.abs(pa[0]-pa0+[-360,0,360]))
+	# 			if a == 0:
+	# 				pa -= 360
+	# 			elif a == 2:
+	# 				pa += 360
 
-			# Optimizing PA
-			# cut = np.arange(360,0,-10)
-			# r = np.array([list(pa)]*len(cut))
-			# for j, c in enumerate(cut):
-			# 	r[j, np.where(r[j,:] > c)[0]] -=360
-			# l = np.argmin(np.ptp(r,axis=1))
-			# pa = r[l]
+	# 		# Optimizing PA
+	# 		# cut = np.arange(360,0,-10)
+	# 		# r = np.array([list(pa)]*len(cut))
+	# 		# for j, c in enumerate(cut):
+	# 		# 	r[j, np.where(r[j,:] > c)[0]] -=360
+	# 		# l = np.argmin(np.ptp(r,axis=1))
+	# 		# pa = r[l]
 
-			# Finding smoothest pa by add or subtracting 360 deg	
-			for j in range(1,len(pa)):
-			    test = np.array([pa[j]-360, pa[j], pa[j]+360])
-			    a = np.argmin(np.abs(test-pa[j-1]))
-			    if a==0: 
-			        pa[j:]-=360
-			    elif a==2: 
-			        pa[j:]+=360
+	# 		# Finding smoothest pa by add or subtracting 360 deg	
+	# 		for j in range(1,len(pa)):
+	# 		    test = np.array([pa[j]-360, pa[j], pa[j]+360])
+	# 		    a = np.argmin(np.abs(test-pa[j-1]))
+	# 		    if a==0: 
+	# 		        pa[j:]-=360
+	# 		    elif a==2: 
+	# 		        pa[j:]+=360
 			
 
 
-			pl = ax.plot(rad,pa,colors[i],label='%s PA' % (type))
-			pl_ob.append(pl)
+	# 		pl = ax.plot(rad,pa,colors[i],label='%s PA' % (type))
+	# 		pl_ob.append(pl)
 
-			# Plot k1 from velocity fit.
-			if type == 'vel':
-				ax2=ax.twinx()
-				b=ax2.plot(rad,k1,'r', label='k1')
-				ax2.spines['right'].set_color('red')
-				ax2.yaxis.label.set_color('red')
-				ax2.tick_params(axis='y', colors='red')
-				ax2.set_ylabel('k1',color='r', rotation=270, labelpad=10)
+	# 		# Plot k1 from velocity fit.
+	# 		if type == 'vel':
+	# 			ax2=ax.twinx()
+	# 			b=ax2.plot(rad,k1,'r', label='k1')
+	# 			ax2.spines['right'].set_color('red')
+	# 			ax2.yaxis.label.set_color('red')
+	# 			ax2.tick_params(axis='y', colors='red')
+	# 			ax2.set_ylabel('k1',color='r', rotation=270, labelpad=10)
 
-				ax2.spines['left'].set_color('blue')
+	# 			ax2.spines['left'].set_color('blue')
 
-		else:
-			print 'There is no %s KINEMETRY file for %s.' %(type, gal)
-			missing +=1
+	# 	else:
+	# 		print 'There is no %s KINEMETRY file for %s.' %(type, gal)
+	# 		missing +=1
 
-	# If not all missing
-	if missing != 3:
-		# Get redshift of galaxy from data_file
-		data_file =  "%s/Data/vimos/analysis/galaxies.txt" % (cc.base_dir)
-		classify_file = "%s/Data/muse/analysis/galaxies_classify.txt" % (cc.base_dir)
+	# # If not all missing
+	# if missing != 3:
+	# 	# Get redshift of galaxy from data_file
+	# 	data_file =  "%s/Data/vimos/analysis/galaxies.txt" % (cc.base_dir)
+	# 	classify_file = "%s/Data/muse/analysis/galaxies_classify.txt" % (
+	#		cc.base_dir)
 
-		# different data types need to be read separetly
-		z_gals = np.loadtxt(data_file, unpack=True, skiprows=1, usecols=(1,))
-		galaxy_gals = np.loadtxt(data_file, skiprows=1, usecols=(0,),dtype=str)
-		i_gal = np.where(galaxy_gals==gal)[0][0]
-		z = z_gals[i_gal]
+	# 	# different data types need to be read separetly
+	# 	z_gals = np.loadtxt(data_file, unpack=True, skiprows=1, usecols=(1,))
+	# 	galaxy_gals = np.loadtxt(data_file, skiprows=1, usecols=(0,),dtype=str)
+	# 	i_gal = np.where(galaxy_gals==gal)[0][0]
+	# 	z = z_gals[i_gal]
 
-		# Set secondary x axis in kpc.
-		ax3=ax.twiny()
-		c = 299792 #km/s
-		#H = 67.8 #(km/s)/Mpc # From Planck
-		H = 70.0 # value used by Bolonga group.
-		r_kpc = np.radians(rad/(60.0*60.0)) * z*c/H *1000
-		ax3.set_xlim(r_kpc[0],r_kpc[-1])
-		ax3.set_xlabel('Radius (kpc)')
+	# 	# Set secondary x axis in kpc.
+	# 	ax3=ax.twiny()
+	# 	c = 299792 #km/s
+	# 	#H = 67.8 #(km/s)/Mpc # From Planck
+	# 	H = 70.0 # value used by Bolonga group.
+	# 	r_kpc = np.radians(rad/(60.0*60.0)) * z*c/H *1000
+	# 	ax3.set_xlim(r_kpc[0],r_kpc[-1])
+	# 	ax3.set_xlabel('Radius (kpc)')
 
-		#ax.yaxis.label.set_color('blue')
-		#ax.tick_params(axis='y', colors='blue')
-		ax.set_ylabel('PA')#,color='b')
-		ax.set_xlabel('radius (arcsec)')
+	# 	#ax.yaxis.label.set_color('blue')
+	# 	#ax.tick_params(axis='y', colors='blue')
+	# 	ax.set_ylabel('PA')#,color='b')
+	# 	ax.set_xlabel('radius (arcsec)')
 
-		# Mark extent of KDC
-		galaxy_gals2, KDC_size = np.loadtxt(classify_file, unpack=True, usecols=(0,5), 
-		dtype=str, skiprows=1)
-		has_KDC = KDC_size!='-'
-		galaxy_gals2 = galaxy_gals2[has_KDC]
-		KDC_size = KDC_size[has_KDC].astype(float)
+	# 	# Mark extent of KDC
+	# 	galaxy_gals2, KDC_size = np.loadtxt(classify_file, unpack=True, 
+	# 		usecols=(0,5), dtype=str, skiprows=1)
+	# 	has_KDC = KDC_size!='-'
+	# 	galaxy_gals2 = galaxy_gals2[has_KDC]
+	# 	KDC_size = KDC_size[has_KDC].astype(float)
 
-		if gal in galaxy_gals2:
-			ax.axvline(KDC_size[galaxy_gals2==gal][0])
+	# 	if gal in galaxy_gals2:
+	# 		ax.axvline(KDC_size[galaxy_gals2==gal][0])
 
-		# Legend
-		try:
-			lns = b
-			for l in pl_ob: lns+=l
-		except UnboundLocalError:
-			if missing==1:lns=pl_ob[0]+pl_ob[1]
-			else:lns=pl_ob[0]
+	# 	# Legend
+	# 	try:
+	# 		lns = b
+	# 		for l in pl_ob: lns+=l
+	# 	except UnboundLocalError:
+	# 		if missing==1:lns=pl_ob[0]+pl_ob[1]
+	# 		else:lns=pl_ob[0]
 
-		labs = [l.get_label() for l in lns]
-		ax.legend(lns, labs, loc=0, facecolor='w')
+	# 	labs = [l.get_label() for l in lns]
+	# 	ax.legend(lns, labs, loc=0, facecolor='w')
 
-		# Moves title clear of upper x axis 
-		plt.subplots_adjust(top=0.85)
-		ax.set_title('KINEMETRY gas output (smoothed)', y=1.12)
+	# 	# Moves title clear of upper x axis 
+	# 	plt.subplots_adjust(top=0.85)
+	# 	ax.set_title('KINEMETRY gas output (smoothed)', y=1.12)
 
-		fig.savefig('%s/%s/%s/kinemetry/kinemetry.png'%(out_dir, gal, opt))
+	# 	fig.savefig('%s/%s/%s/kinemetry/kinemetry.png'%(out_dir, gal, opt))
 
-	plt.close()
+	# plt.close()
 
 
-	Prefig(size=(16*3,12*5), transparent=False)
-	fig, ax = plt.subplots(5,3)
+	Prefig(size=(4*16,12), transparent=False)
+	fig, ax = plt.subplots(1,5)
 	fig.suptitle('Kinemetry fit to Ionised gas dynamics')
 
 	f = fits.open(get_dataCubeDirectory(gal))
@@ -146,51 +149,71 @@ def use_kinemetry(gal, opt='kin'):
 	header['NAXIS1'] = 150
 	header['NAXIS2'] = 150
 
-	tessellation_File = "%s/%s/%s/setup/voronoi_2d_binning_output.txt" % (out_dir, 
-		gal, opt)
+	tessellation_File = "%s/%s/%s/setup/voronoi_2d_binning_output.txt" % (
+		out_dir, gal, opt)
 	x,y,bin_num = np.loadtxt(tessellation_File, usecols=(0,1,2), 
 		unpack=True, skiprows=1, dtype=int)
 
-	for i, type in enumerate(['flux','vel','sigma']):
-		f = '%s/%s/%s/kinemetry/kinemetry_gas_%s_2Dmodel.txt' % (out_dir, gal, opt, type)
-		xbin, ybin, velkin, velcirc  = np.loadtxt(f, unpack=True, skiprows=1)
+	f = '%s/%s/%s/kinemetry/kinemetry_gas_vel_2Dmodel.txt' % (out_dir, gal, 
+		opt)
+	# f = '%s/%s/%s/kinemetry/kinemetry_stellar_vel_2Dmodel.txt' % (out_dir, gal, 
+	# 	'kin')
+	xbin, ybin, velkin, velcirc  = np.loadtxt(f, unpack=True, skiprows=1)
 
-		f = '%s/%s/%s/kinemetry/gas_%s.dat' % (out_dir, gal, opt, type)
-		vel = np.loadtxt(f, usecols=(0,), unpack=True)
-		vel[vel==9999] = np.nan
+	f = '%s/%s/%s/kinemetry/gas_vel.dat' % (out_dir, gal, opt)
+	vel = np.loadtxt(f, usecols=(0,), unpack=True)
+	m = np.where(vel!=9999)[0]
+	# x = [i for j, i in enumerate(x) if bin_num[j] in m]
+	# y = [i for j, i in enumerate(y) if bin_num[j] in m]
+	# bin_num = [i for j, i in enumerate(bin_num) if bin_num[j] in m]
+	# vel = vel[vel != 9999]
 
-		velkin[velkin==max(velkin)] = np.nan
-		velcirc[velcirc==max(velcirc)] = np.nan
+	vel[vel==9999] = np.nan
+	velkin_new = vel*0
+	velcirc_new = vel*0
+	j = 0
+	for i in range(len(vel)):
+		if ~np.isnan(vel[i]):
+			velkin_new[i] = velkin[j]
+			velcirc_new[i] = velcirc[j]
+			j += 1
+	velkin = velkin_new
+	velcirc = velcirc_new	
 
-		# f = '%s/%s/%s/kinemetry/gas_flux.dat' % (out_dir, gal, opt)
-		# flux = np.loadtxt(f)
 
-		vmin, vmax = set_lims(vel, symmetric=type=='vel', positive=type!='vel')
-		plot_velfield_nointerp(x, y, bin_num, xbin, ybin, 
-			vel, header, vmin=vmin, vmax=vmax, nodots=True, 
-			title='%s Data'%(type), colorbar=True, ax=ax[0,i])#, flux=flux)
-		
+	velkin[velkin==max(velkin)] = np.nan
+	velcirc[velcirc==max(velcirc)] = np.nan
 
-		plot_velfield_nointerp(x, y, bin_num, xbin, ybin, 
-			velkin, header, vmin=vmin, vmax=vmax, nodots=True, title='KINEMETRY %s model'
-			% (type), colorbar=True, ax=ax[1,i])#, flux=flux)
+	# f = '%s/%s/%s/kinemetry/gas_flux.dat' % (out_dir, gal, opt)
+	# flux = np.loadtxt(f)
 
-		plot_velfield_nointerp(x, y, bin_num, xbin, ybin, 
-			velcirc, header, vmin=vmin, vmax=vmax, nodots=True, 
-			title='KINEMETRY circluar %s'
-			% (type), colorbar=True, ax=ax[3,i])#, flux=flux)
+	vmin, vmax = set_lims(vel, symmetric=True, positive=False)
+	plot_velfield_nointerp(x, y, bin_num, xbin, ybin, 
+		vel, header, vmin=vmin, vmax=vmax, nodots=True, 
+		title='Observed Velocity', colorbar=False, ax=ax[0])
 
-		vmin, vmax = set_lims(vel-velkin, symmetric=True)
-		plot_velfield_nointerp(x, y, bin_num, xbin, ybin, 
-			vel-velkin, header, vmin=vmin, vmax=vmax, nodots=True, 
-			title='KINEMETRY %s vkin residuals'%(type), 
-			colorbar=True, ax=ax[2,i])#, flux=flux)
+	plot_velfield_nointerp(x, y, bin_num, xbin, ybin, 
+		velkin, header, vmin=vmin, vmax=vmax, nodots=True, 
+		title='KINEMETRY velocity model', colorbar=False, ax=ax[1])
 
-		vmin, vmax = set_lims(vel-velcirc, symmetric=True)
-		plot_velfield_nointerp(x, y, bin_num, xbin, ybin, 
-			vel-velcirc, header, vmin=vmin, vmax=vmax, nodots=True, 
-			title='KINEMETRY %s vcirc residuals'%(type), 
-			colorbar=True, ax=ax[4,i])#, flux=flux)
+	plot_velfield_nointerp(x, y, bin_num, xbin, ybin, 
+		velcirc, header, vmin=vmin, vmax=vmax, nodots=True, 
+		title='KINEMETRY circluar velocity model', colorbar=False, ax=ax[3])
+
+	# vmin, vmax = set_lims(vel-velkin, symmetric=True)
+	plot_velfield_nointerp(x, y, bin_num, xbin, ybin, 
+		vel-velkin, header, vmin=vmin, vmax=vmax, nodots=True, 
+		title='KINEMETRY model residuals', colorbar=False, ax=ax[2])
+
+	# vmin, vmax = set_lims(vel-velcirc, symmetric=True)
+	plot_velfield_nointerp(x, y, bin_num, xbin, ybin, 
+		vel-velcirc, header, vmin=vmin, vmax=vmax, nodots=True, 
+		title='KINEMETRY circluar model residuals', colorbar=True, ax=ax[4])
+
+	for a in ax:
+		for o, c in {'radio':'g','CO':'c'}.iteritems():
+			add_(o, c, a, gal, scale='log')#, radio_band='L')
+	
 
 
 	fig.savefig('%s/%s/%s/kinemetry/kinemetry_models.png'%(out_dir, gal, opt))
@@ -198,7 +221,7 @@ def use_kinemetry(gal, opt='kin'):
 
 
 	
-##############################################################################
+############################################################################
 
 # Use of plot_absorption.py
 
@@ -206,4 +229,4 @@ if __name__ == '__main__':
 	# for gal in ['ic1459', 'ic4296', 'ngc1316', 'ngc1399']:
 	# 	print gal
 	# 	use_kinemetry(gal)
-	use_kinemetry('ngc1399', opt='kin')
+	use_kinemetry('ngc1316', opt='pop')
